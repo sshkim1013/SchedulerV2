2f55beae0020c2f38d9e98019c90896dc52087ce
Kim su-hwan <sshkim1013@naver.com>
2025-02-10 17:44:09 +0900

    add filter

---
+++ src/main/java/com/example/schedulerv2/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.example.schedulerv2.config;
+
+import com.example.schedulerv2.filter.CustomFilter;
+import com.example.schedulerv2.filter.LoginFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean customFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        // Filter 등록
+        filterRegistrationBean.setFilter(new CustomFilter());
+        // Filter 순서 설정
+        filterRegistrationBean.setOrder(1);
+        // 전체 URL에 필터 적용
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+}
---
+++ src/main/java/com/example/schedulerv2/filter/LoginFilter.java
@@ -0,0 +1,51 @@
+package com.example.schedulerv2.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFilter implements Filter {
+
+    private static final String[] URL_LIST = {"/users"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        log.info("로그인 필터 로직 실행!");
+
+        //WHITE_LIST 내에 포함된 경우 true -> !true -> false
+        if (!isWhiteList(requestURI)) {
+
+            HttpSession session = httpRequest.getSession(false);
+
+            if (session == null || session.getAttribute("sessionKey") == null) {
+                throw new RuntimeException("로그인을 해주시기 바랍니다.");
+            }
+
+            //로그인 성공 로직
+            log.info("로그인 성공!");
+
+        }
+
+        //1번 경우 : WHITE_LIST에 등록된 URL 요청이라면, 바로 filterChain.doFilter() 호출.
+        //2번 경우 : WHITE_LIST가 아닌 경우, 위 필터 로직을 통과한 후에 filterChain.doFilter() 다음 서블릿 또는 필터를 호출.
+        //다음 필터가 없으면 servlet -> controller, 다음 필터가 있으면 다음 Filter 호출.
+        filterChain.doFilter(request, response);
+
+    }
+
+    //WHITE_LIST 배열의 값과 일치하는지 검증하는 로직
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(URL_LIST, requestURI);
+    }
+}
